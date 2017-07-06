b0bafa29fcf93ea455a763c2df6abf6583bac3cd
Cordel Murphy <cmurph20@myune.edu.au>
2017-07-06 22:53:46 +1000

    Starting Files

---
+++ Desktop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cosmos.System.Graphics;
+
+namespace StarOS
+{
+    public class Desktop
+    {
+        internal static void startGUI ()
+        {
+            Canvas canvas;
+            canvas = FullScreenCanvas.GetFullScreenCanvas();
+            canvas.Clear(Color.FromArgb(200,54,82));
+        }
+    }
+}
---
+++ Kernel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+
+namespace StarOS
+{
+    public class Kernel: Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("StarOS booted successfully. Type a line of text to get it echoed back.");
+            //Desktop.startGUI();
+        }
+        
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
